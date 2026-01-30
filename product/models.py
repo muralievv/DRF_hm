3d34faaae189c805524845284098d0f801ae6e46
muralievv <nmuraliev@gmail.com>
2026-01-30 17:31:32 +0600

    hm2

--- product/models.py
+++ product/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+    
+    @property
+    def products_count(self):
+        return self.product_set.count()
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -14,9 +20,25 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def average_rating(self):
+        reviews = self.reviews.all()
+        count = reviews.count()
+        if count > 0:
+            total_stars = sum([review.stars for review in reviews])
+            return round(total_stars / count, 1) 
+        
+        return 0
+
 class Review(models.Model):
     text = models.TextField(null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, related_name='reviews')
+    stars = models.IntegerField(default=0, validators=[
+        MinValueValidator(1),
+        MaxValueValidator(5),
+    ])
+  
 
     def __str__(self):
-        return self.text
+        return self.text 
+
--- product/serializers.py
+++ product/serializers.py
@@ -2,17 +2,27 @@ from rest_framework import serializers
 from .models import Category, Product, Review
 
 class CategoryListSerializer(serializers.ModelSerializer):
+    products_count = serializers.ReadOnlyField()
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'products_count']
 
 class ProductListSerializer(serializers.ModelSerializer):
+    category = serializers.ReadOnlyField(source='category.name')
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'title','description','category', 'average_rating']
     
 
 class ReviewListSerializer(serializers.ModelSerializer):
+    average_rating = serializers.ReadOnlyField(source='product.average_rating')
+    
+    product = serializers.ReadOnlyField(source='product.title')
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = ['text', 'product', 'stars', 'average_rating']
+
+    
+    
+
+
